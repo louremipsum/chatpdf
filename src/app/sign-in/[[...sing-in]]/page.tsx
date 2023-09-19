8b20385fb7a1c422a10e00bd842187ed123451e9
louremipsum <myself.vinayak2@gmail.com>
2023-09-19 11:06:26 +0530

    feat: clerkProvider w auth pages

---
+++ src/app/sign-in/[[...sing-in]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="w-screen min-h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-300 to-purple-400">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <SignIn />
+        </div>
+      </div>
+    </div>
+  );
+}
