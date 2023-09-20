4e606f3541e3cd95c4ae0a8aebea5f8b4ffda457
louremipsum <myself.vinayak2@gmail.com>
2023-09-20 15:36:31 +0530

    fix: comments & spaces

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -6,18 +6,24 @@ import { uploadToS3 } from "@/lib/s3";
 
 const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
+    // all accepted formats
     accept: { "application/pdf": [".pdf"] },
+    // number of files which can be uploaded once
     maxFiles: 1,
+    // what will happen after dropping or giving a file to that component
     onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+
+      // accessing [0] as the accepted file comes in an array format with actual file desc in [0] and prototype at [1]
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        // bigger than 10mb!
+        // if file size bigger than 10mb then don't upload!
         alert("File size is bigger than 10mb");
         return;
       }
 
       try {
+        // uploading the file to S3 using uploadToS3 fn
         const data = await uploadToS3(file);
         console.log("data: ", data);
       } catch (error) {
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -6,24 +6,30 @@ export async function uploadToS3(file: File) {
       accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
     });
+
     const s3 = new AWS.S3({
       params: {
         Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
       },
       region: "ap-south-1",
     });
+
+    // making a unique(by date.now fn) file key for each file
     const file_key =
       "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
 
+    // what would actually be uploaded to S3
     const params = {
       Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
       Key: file_key,
       Body: file,
     };
 
+    // Promise to actually upload(putObject) to S3 along with a progress emitter for frontend
     const upload = s3
       .putObject(params)
       .on("httpUploadProgress", (evt) => {
+        // for making a progress bar to show in UI of upload progress
         console.log(
           "uploading to s3...",
           parseInt(((evt.loaded * 100) / evt.total).toString() + "%")
@@ -31,7 +37,9 @@ export async function uploadToS3(file: File) {
       })
       .promise();
 
+    // uploading the file to S3
     await upload.then((data) => {
+      // this part runs after uploaded so we can change state in UI or redirect to next page from here
       console.log("successfully uploaded to S3!: " + file_key);
     });
 
@@ -45,6 +53,7 @@ export async function uploadToS3(file: File) {
 }
 
 export function getS3Url(file_key: string) {
+  // get url of that file in S3 Bucket to show it in UI while chatting
   const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-south-1.amazonaws.com/${file_key}`;
   return url;
 }
